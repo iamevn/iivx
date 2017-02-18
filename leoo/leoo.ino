681c38696a7cb865d0bb851addc8e3de42d80c1d
Sean Rhone <Espionage724@users.noreply.github.com>
2017-02-18 01:09:16 -0500

    Update leoo.ino
    - Switch to HID lighting by-default
    - Fix for Button 9's HID light

--- leoo/leoo.ino
+++ leoo/leoo.ino
@@ -9,7 +9,7 @@ iivxReport_t report;
 
 int tmp;
 uint8_t buttonCount = 9;
-uint8_t lightMode = 0;
+uint8_t lightMode = 1;
 // 0 = reactive lighting, 1 = HID lighting
 // HID lighting is supported for bemanitools with pop'n music and DanceDanceRevolution
 // Reactive lighting activates lights on button-press, regardless of the game (StepMania)
@@ -29,7 +29,7 @@ uint8_t sysPin = 0;
  *    system button + button 3 = HID lighting
  */
 
-void lights(uint8_t lightDesc){
+void lights(uint16_t lightDesc){
   for(int i=0;i<buttonCount;i++){
      if((lightDesc>>i)&1){
          digitalWrite(ledPins[i],HIGH);
@@ -71,10 +71,17 @@ void loop() {
     } else if (report.buttons == 4){
       lightMode = 1;
     } else if (report.buttons == 16){
+      report.buttons = 0;
       report.buttons = (uint16_t)1 << 9;
     } else if (report.buttons == 64){
+      report.buttons = 0;
       report.buttons = (uint16_t)1 << 10;
+    } else {
+      report.buttons = 0;
     }
+  } else {
+    report.buttons &= ~((uint16_t)1 << 9);
+    report.buttons &= ~((uint16_t)1 << 10);
   }
   // Send report and delay
   iivx.setState(&report);
